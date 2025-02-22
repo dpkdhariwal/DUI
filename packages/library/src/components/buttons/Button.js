a12b3bbe77c08fe244e286401c38012b465ea670
Deepak Dhariwal <dpkdhariwal@gmail.com>
2025-02-22 16:54:05 +0530

    #

--- packages/library/src/components/buttons/Button.js
+++ packages/library/src/components/buttons/Button.js
@@ -1,23 +1,39 @@
 import { ref, h, defineComponent } from 'vue'
 
 export const Dbutton = defineComponent(
-  (props) => {
+  (props, { slots }) => {
     // use Composition API here like in <script setup>
     const count = ref(0)
 
     const handleClick = () => {
       count.value++
+      console.log('Button clicked', count.value)
     }
 
     return () => {
       // render function or JSX
-      return h('button', { class: 'dbutton', onClick: handleClick }, count.value)
+      return h(
+        'button',
+        { class: 'dbutton', onClick: handleClick, role: 'button', style: { width: props.width } },
+        [
+          slots.default ? slots.default() : props.title, // Named slot "title"
+          h('br'), // Line break for separation
+          slots.description ? slots.description() : '', // Named slot "description" (optional)
+        ],
+      )
     }
   },
   // extra options, e.g. declare props and emits
   {
     props: {
-      /* ... */
+      width: {
+        type: String,
+        default: '200px',
+      },
+      title: {
+        type: String,
+        default: 'Click me',
+      },
     },
   },
 )
